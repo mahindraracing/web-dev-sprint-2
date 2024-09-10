5d4f3d86693f61df0bd44548ce836d571d926096
imneli <contatodoneli@gmail.com>
2024-09-10 09:29:50 -0300

    btn + Link to

--- src/screens/Error/index.jsx
+++ src/screens/Error/index.jsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
+
 function Error() {
     return(
         <>
-        <div className="bg-red-500 h-[100vh] flex justify-center items-center">
+        <div className="bg-red-500 h-[100vh] flex justify-center items-center flex-col">
             <h1 className="font-bold text-[200px] text-white">404</h1>
+            <Button className="bg-white text-black" ><Link to="/">Return to Home</Link></Button>
         </div>
-            
         </>
     )
 }
