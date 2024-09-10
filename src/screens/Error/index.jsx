4bc4d510a95433a217ae9dc91d357d14559c1784
imneli <contatodoneli@gmail.com>
2024-09-10 09:25:22 -0300

    error page

--- src/screens/Error/index.jsx
+++ src/screens/Error/index.jsx
@@ -2,7 +2,7 @@ function Error() {
     return(
         <>
         <div className="bg-red-500 h-[100vh] flex justify-center items-center">
-            <h1 className="font-bold text-[300px] text-white">404</h1>
+            <h1 className="font-bold text-[200px] text-white">404</h1>
         </div>
             
         </>
